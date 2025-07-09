574ff0064864f4a85165ef6a485501168ccd82a6
Sanjay <sanjaysaravanan200420@gmail.com>
2025-07-09 09:58:33 +0530

    Simple To-Do-List

---
+++ To-Do-List.js
@@ -0,0 +1,14 @@
+let taskInput = document.querySelector("input");
+let addButton = document.getElementById("btn");
+let taskList = document.querySelector("ul");
+
+addButton.addEventListener("click", function () {
+  let taskText = taskInput.value;
+
+  if (taskText !== "") {
+    let newTask = document.createElement("li");
+    newTask.innerText = taskText;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  }
+});
