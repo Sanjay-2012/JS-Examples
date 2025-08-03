70016f904fb455025d18fbaba7a797f78b3a4425
Sanjay <sanjaysaravanan200420@gmail.com>
2025-08-03 20:52:41 +0530

    Update To-Do-List.js

--- To-Do-List.js
+++ To-Do-List.js
@@ -4,7 +4,6 @@ let taskList = document.querySelector("ul");
 
 addButton.addEventListener("click", function () {
   let taskText = taskInput.value;
-
   if (taskText !== "") {
     let newTask = document.createElement("li");
     newTask.innerText = taskText;
